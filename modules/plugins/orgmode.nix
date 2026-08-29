c644298fd1a41a367144fd701050d7c02018d429
Ziteng Wang <z@wzt.me>
2026-08-29 00:02:16 -0500

    Configure org tag inheritance

--- modules/plugins/orgmode.nix
+++ modules/plugins/orgmode.nix
@@ -5,6 +5,7 @@
     settings = {
       org_agenda_files = "~/personal/orbit/**/*";
       org_default_notes_file = "~/personal/orbit/inbox.org";
+      org_tags_exclude_from_inheritance = ["project"];
       org_todo_keywords = [
         "TODO"
         "NEXT"
