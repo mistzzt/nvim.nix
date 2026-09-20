0be5a15bf97f01980d2c0720e2f29f060f71b4d3
Ziteng Wang <z@wzt.me>
2026-09-19 21:56:17 -0500

    Set main Beancount journal for language server

--- modules/languages/beancount.nix
+++ modules/languages/beancount.nix
@@ -1,7 +1,10 @@
 {config, ...}: {
   filetype.extension.bean = "beancount";
 
-  lsp.servers.beancount.enable = true;
+  lsp.servers.beancount = {
+    enable = true;
+    config.init_options.journal_file = "~/personal/bcac/main.bean";
+  };
 
   plugins.treesitter.grammarPackages = [
     config.plugins.treesitter.package.builtGrammars.beancount
